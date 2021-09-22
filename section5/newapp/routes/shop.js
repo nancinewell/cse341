06ab844393a8e21181739832630b1e9ef37c293e
nancinewell <nanci.newell@gmail.com>
2021-09-21 22:46:22 -0500

    update

--- section5/newapp/routes/shop.js
+++ section5/newapp/routes/shop.js
@@ -9,8 +9,8 @@ const router = express.Router();
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    console.log('shop.js', adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
