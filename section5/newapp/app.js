a3f9fec2c6e158f2faef47bb6b5806f44587419d
nancinewell <nanci.newell@gmail.com>
2021-09-22 13:19:01 -0500

    commit

--- section5/newapp/app.js
+++ section5/newapp/app.js
@@ -2,14 +2,17 @@ const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
-const expressHbs = require('express-handlebars');
+
+//const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs);
-app.set('view engine', 'hbs');
+//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));// 'views/layouts' is the default. Reset it if you need to use a different location
+//app.set('view engine', 'hbs');
 
 //app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
 
 app.set('views', 'views'); //views is actually the default. Be sure to set this if you use another folder. 
 
