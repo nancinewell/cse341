484eaf70a10020f406c1fb331f66fe205553c3b6
nancinewell <nanci.newell@gmail.com>
2021-09-21 21:17:58 -0500

    update

---
+++ section5/newapp/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
+
+
