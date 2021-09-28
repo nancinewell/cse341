c83785b22818c493f7ca96b3b8ddd6decccf9e8b
nancinewell <nanci.newell@gmail.com>
2021-09-27 20:49:58 -0500

    finished Adding A Cart Model

--- w3prepare/controllers/shop.js
+++ w3prepare/controllers/shop.js
@@ -1,47 +1,64 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-// * * * * * * * GET PRODUCTS * * * * * * * 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
+  Product.fetchAll(products => {
     res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products',
-        });
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
     });
+  });
 };
 
-// * * * * * * * GET INDEX * * * * * * * 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    console.log("in controller-getProduct- prodId: " + prodId);
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products'});
+    });
+    
+}
+
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            });
-        });
-}
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
 
-// * * * * * * * GET CART * * * * * * * 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
-    });
-}
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
 
-// * * * * * * * GET ORDERS * * * * * * * 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log("shop.js controller  exports.postCart prodId: " + prodId);
+    Product.findById(prodId, product => {
+        Cart.addProduct(prodId, product.price);
+    })
+    res.redirect('/cart');
+
+  };
+  
+
+
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    });
-}
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 
-// * * * * * * * GET CHECKOUT * * * * * * * 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
-    });
-}
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
