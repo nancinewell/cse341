fa8e8f91bfec41d0227f4fd4469e557a4bbf6784
nancinewell <nanci.newell@gmail.com>
2021-10-20 20:50:01 -0500

    commit

--- w6prepare/app.js
+++ w6prepare/app.js
@@ -28,6 +28,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'aLongStringValue', resave: false, saveUninitialized: false, store: store}));
@@ -36,31 +37,38 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if(!req.session.user){
-    return next();
-  }
-  User.findById(req.session.user._id)
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => console.log(err));
-});
-
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken= req.csrfToken();
   next();
 });
 
+app.use((req, res, next) => {
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
+      .then(user => {
+        if(!user){
+          return next();
+        }
+        req.user = user;
+        next();
+      })
+      .catch(err => {next(new Error(err))});
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use(errorController.get404);
+app.get(errorController.get500);
+//or
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+  //or render with status code or anything else.
+})
 
+app.use(errorController.get404);
 
 const config = {
   autoIndex: false,
--- w6prepare/controllers/admin.js
+++ w6prepare/controllers/admin.js
@@ -1,11 +1,14 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn,
+    hasError: false,
+    errorMessage: null
     //or isAuthenticated: req.session.user;
   });
 };
@@ -15,7 +18,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  console.log(req.user)
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      isAuthenticated: req.session.isLoggedIn,
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    });
+  }
   const product = new Product({
     title: title,
     price: price,
@@ -31,7 +51,30 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      console.log(err);
+      
+      //  //Return same view with info already input
+      // return res.status(500).render('admin/edit-product', {
+      //   pageTitle: 'Add Product',
+      //   path: '/admin/edit-product',
+      //   editing: false,
+      //   product: {
+      //     title: title,
+      //     imageUrl: imageUrl,
+      //     price: price,
+      //     description: description
+      //   },
+      //   isAuthenticated: req.session.isLoggedIn,
+      //   hasError: true,
+      //   errorMessage: "Product was not added due to a database error. Please try again later.",
+      //   validationErrors: []
+      // });
+
+      // //Redirect to error page
+      //res.redirect('/500');
+
+      const error= new Error('something happened...');
+      error.httpStatusCode = 500;
+      return next(error); //When you call next() with an error as an argument, it'll skip all other middlewares and go to an error-handling middleware.(app.js)
     });
 };
 
@@ -51,7 +94,9 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn,
+        hasError: false,
+        errorMessage: null
       });
     })
     .catch(err => console.log(err));
@@ -63,6 +108,24 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc
+      },
+      isAuthenticated: req.session.isLoggedIn,
+      hasError: true,
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   Product.findById(prodId)
     //only allow edits if user is the creator of the product
@@ -96,7 +159,9 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.session.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn,
+        hasError: false,
+        errorMessage: null
       });
     })
     .catch(err => console.log(err));
